65b60c58552ba2354ef01d5f6af41f1fc6ab82e4
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-02 18:33:48 +0100

    es. completo senza bonus

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ for (let i = 1; i <= 100; i++) {
     eleCell.classList.add("cell");
     eleCell.innerHTML += i; 
     eleGrid.append(eleCell);  
+
+    eleCell.addEventListener("click", function(){
+        eleCell.classList.toggle("active");
+        console.log("Hai selezionato la casella numero " + i);
+        
+        // if (eleCell.classList.add("active")) {
+        //     console.log("Hai selezionato la casella numero " + i);
+        // } else if (eleCell.classList.remove("active")) {
+        //     console.log(`Hai deselezionato la casella numero ${i}`);
+        // }
+    })
 }
