f98b0dadac887bd0455c1dc81305e9acbb853aee
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-02 18:14:15 +0100

    cell numerata creata

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@ const eleGrid = document.querySelector(".grid");
 
 
 for (let i = 1; i <= 100; i++) {
-    // const eleCell = document.createElement("div");
-    // eleCell.classList.add("cell");
-    // eleGrid.append(eleCell[i]);                      //non funziona
-
-    eleGrid.innerHTML += `<div class="cell"> ${i} </div>` //funziona
+    const eleCell = document.createElement("div");
+    eleCell.classList.add("cell");
+    eleCell.innerHTML += i; 
+    eleGrid.append(eleCell);  
 }
